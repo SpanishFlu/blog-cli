23579735740492fa9900ff19a8c226eac2d11193
SpanishFlu <saifeddinesayed@gmail.com>
2026-01-31 00:10:41 +0200

    sdsf

--- main.go
+++ main.go
@@ -4,14 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("Blog CLI start")
-	fmt.Println("==== Blogging Platform ====
-				1. Create Post
-				2. View All Posts
-				3. View Post by ID
-				4. Update Post
-				5. Delete Post
-				6. Search Posts
-				0. Exit
+	fmt.Printf("==== Blogging Platform ====\n 1. Create Post\n
+				2. View All Posts\n
+				3. View Post by ID\n
+				4. Update Post\n
+				5. Delete Post\n
+				6. Search Posts\n
+				0. Exit\n
 				Choose an option:"
 				)
 }
