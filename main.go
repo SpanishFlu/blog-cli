a5df2543dac4416abb36e6c1239e1ddb9fdae994
SpanishFlu <saifeddinesayed@gmail.com>
2026-01-31 00:06:23 +0200

    sdsf

--- main.go
+++ main.go
@@ -4,4 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Println("Blog CLI start")
+	fmt.Println("==== Blogging Platform ====
+				1. Create Post
+				2. View All Posts
+				3. View Post by ID
+				4. Update Post
+				5. Delete Post
+				6. Search Posts
+				0. Exit
+				Choose an option:"
+				)
 }
